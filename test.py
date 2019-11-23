2ddffaa7d47bd9cb3f07f1cead7262f2899a42ad
hejunwang <287619892@qq.com>
2019-11-23 13:34:14 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: toby
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: hejunwang01@gmail.com
+@software: pycharm 
+@file: test.py
+@time: 2019/11/23 13:27
+@desc:
+'''
+
+'''
+在GitHub上创建仓库   
+vcs -->import into version --> share project on github 
+
+
+'''
