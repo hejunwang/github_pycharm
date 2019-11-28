9c8d02c8d215c7c85959bd12d593b6ac5c2bbd9e
hejunwang <287619892@qq.com>
2019-11-28 14:02:50 +0800

    fiddler 抓包的分析(1)

---
+++ zhuabao.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: toby
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: hejunwang01@gmail.com
+@software: pycharm 
+@file: zhuabao.py
+@time: 2019/11/28 12:53
+@desc:
+'''
+
+"使用fiddler进行抓包"
+"""
+使用firfox 浏览器 ,设置浏览器的代理 本地代理127.0.0.1 :8888  ,这个时间要打开fiddler代理 ,否则是没有办法上网的
+
+例如抓取淘宝的评价 
+可以在responese的 textview中搜索里面的文字,查找相应的session  就可以看到是具体的url,然后保存起来备用 
+
+"""
