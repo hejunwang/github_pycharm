0334fa1c6404cff3be56da5bd2f32fb5c2108ea3
hejunwang <287619892@qq.com>
2019-12-09 19:45:05 +0800

    numpy 数组的计算2

---
+++ numpy_file/numpy_3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: toby
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: hejunwang01@gmail.com
+@software: pycharm 
+@file: numpy_3.py
+@time: 2019/12/5 19:10
+@desc:
+'''
+
+import numpy as np
+n1 =np.arange(24).reshape(4,6)
+print(n1)
+'''
+[[ 0  1  2  3  4  5]
+ [ 6  7  8  9 10 11]
+ [12 13 14 15 16 17]
+ [18 19 20 21 22 23]]
+'''
+
+#修改里面的数据   bool索引
+print(n1<10)
+'''
+[[ True  True  True  True  True  True]
+ [ True  True  True  True False False]
+ [False False False False False False]
+ [False False False False False False]]
+'''
+n1[n1<15]=3
+print(n1)
