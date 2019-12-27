7d292b2bb924a1d6d5140a76df50dcc42c61c4d2
hejunwang <287619892@qq.com>
2019-12-27 16:46:53 +0800

    scrapy crawl

---
+++ runbook/scrapy_readme.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: toby
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: hejunwang01@gmail.com
+@software: pycharm 
+@file: scrapy_readme.py
+@time: 2019/12/27 16:37
+@desc:
+'''
+
+'''
+scrapy 框架
+
+调度器schedule ,请求的地址, 然后进行地址的数据获取 ,然后到数据的分析 ,最后到下载 
+
+
+'''
+
