9d8f3dc38bdea77792df535924a84c3805960585
hejunwang <287619892@qq.com>
2019-12-27 16:18:59 +0800

    pandas_2

--- pandas/pandas_1.py
+++ pandas/pandas_1.py
@@ -12,9 +12,7 @@
 
 '''
 pandas 处理numpy 之外的 工具,处理原始的数据  比较复杂的 不只是数组类型的 数据  
-
-pandas  的常用数据类型  
-
+pandas的常用数据类型分两类 :  
 Series  一维,带标签的数组  
 DataFrame 二维 ,Series容器  
 
@@ -36,6 +34,12 @@ dtype: int32
 '''
 t1 = pd.Series([1,2,3,4,5],index=list('abcde'))   #指定数组的index
 print(t1)
+# a    1
+# b    2
+# c    3
+# d    4
+# e    5
+# dtype: int64
 
 temp_dict={"name":"abc","age":30,"add":"xsdfsdfsdf"}
 t2 = pd.Series(temp_dict)                      #通过字典的方式 创建
@@ -50,15 +54,26 @@ print('#索引 --- 一个的时间直接传入序号这是index ,多个的时间
       ' 和切片---直接传入start  end  step即可    ')
 name = t2['name']     #通过关键字
 print(name)
+'name           abc'
 
 add = t2[2]          #通过位置 索引
 print(add)
-
+'add     xsdfsdfsdf'
+print("*"*20)
 x1 = t2[:2]     # 取前两行
 print(x1)
+'''
+name    abc
+age      30
+dtype: object
+'''
+
 
 x1 = t2[[0,2]]     # 取不连续的行   t2[['name','add']]  这样也是可以的
 print(x1)
+# name           abc
+# add     xsdfsdfsdf
+
 
 # x1 = t2['tel']     #如果数组中没有对应的关键字,取数据的时间,可能得到nan 或者是报错
 # print(x1)
@@ -68,14 +83,19 @@ x1 = t1[t1>2]
 print(x1)
 
 print(t2.index)       #属性值
+# 'Index(['name', 'age', 'add'], dtype='object')'
+print("*"*100)
 print(t2.values)
-print(type(t2.values))    #<class 'numpy.ndarray'>
+# ['abc' 30 'xsdfsdfsdf']
+print("*"*100)
+print(type(t2.values))
+#<class 'numpy.ndarray'>
 
 
 '''
 读取外部文件   
 '''
-df = pd.read_csv('pand.csv')
+df = pd.read_csv('pandas_1.csv')
 print(df)
 
 
