20cd20ad949699273eb72793ceabb86824e618c4
hejunwang <287619892@qq.com>
2019-12-30 15:25:21 +0800

    matplotlib

---
+++ matplotlib/mat_demo_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: toby
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: hejunwang01@gmail.com
+@software: pycharm 
+@file: mat_demo_2.py
+@time: 2019/12/30 13:17
+@desc:
+'''
+
+'''
+在一定的年龄段 ,交往的女友的个数 
+'''
+a = [1,0,2,3,4,
+     2,2,1,2,3,
+     4,3]
+
+
